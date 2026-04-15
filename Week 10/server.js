7cfed6d2d89b212bb3cb7849bf7952d8d6a93895
Gabrielle Asha <167376964+gabasha00@users.noreply.github.com>
2026-04-15 18:39:31 -0400

    update

--- Week 10/server.js
+++ Week 10/server.js
@@ -1,14 +1,14 @@
 // how do we know this is a npm project?
-// A: Because there is a variable that containes express.
+// A: Because there is a variable that containes express and package.json
 
 // what command do we run to start an npm project?
-// A: We use npm start server.js
+// A: We use npm init -y
 
 // how do we create the node_modules folder if it doesn't exist?
-// A: npm init -y
+// A: npm install
 
 // what does the below chunk of code do?
-// A: It calls all external libraries and puts them into variables to be used
+// A: It imports all external libraries and puts them into variables to be used throughout code
 const express = require('express');
 const multer = require('multer');
 const nunjucks = require('nunjucks');
@@ -22,16 +22,17 @@ const app = express();
 const database = new nedb({ filename: 'data.db', autoload: true });
 
 // what is this configuring?
-// A: Folder that is created once the server is used
+// A: Folder that is created once the server is used, creates folder where assets are stored
 const upload = multer({
 	dest: 'public/uploads',
 });
 
 // what do each of these statements do?
 // write the answer next to the line of code
-app.use(express.static('public')); // A: Says where local host file is
-app.use(express.urlencoded({ extended: true })); // A: Accept all types of input
-app.set('view engine', 'njk'); // A: Templates nunjuck files to express server
+app.use(express.static('public')); // A: Any front-end files from our sites and assets are served.
+// Allows express to expose those files
+app.use(express.urlencoded({ extended: true })); // A: Accept all types of input / any type of file uploaded
+app.set('view engine', 'njk'); // A: Sets express the ability to use and send njk data and file
 nunjucks.configure('views', {
 	autoescape: true,
 	express: app,
@@ -42,14 +43,18 @@ nunjucks.configure('views', {
 // It's requesting the slash route, and responds with the webpage
 app.get('/', (request, response) => {
 	// how many different responses can we write? list them.
-	// A: .json, .send, .render, .response, .redirect
+	// A: .json, .send, .render, .redirect, .sendFile
 	// how many parameters does response.render use? list them.
 	// A: Two, file name and title
 	// write out the render for index.njk using the database
-});
+	// A: 
+	// database.find({}, (err, foundData) => {
+	//	})
+	// response.render('index.njk', {serverData: foundData})
+ });
 
 // what are the three parameters in this function?
-// A: Route, upload.single, callback
+// A: Route (/ upload), upload.single, callback function
 app.post('/upload', upload.single('theimage'), (req, res) => {
 	let currentDate = new Date();
 
@@ -75,7 +80,7 @@ app.post('/upload', upload.single('theimage'), (req, res) => {
 });
 
 // what does the number signify?
-// A: Indicates server number
+// A: Indicates port number
 // how do we access this on the web?
 // A: localhost:6001
 app.listen(6001, () => {
